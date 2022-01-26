a6f96f525141b82fbcc92a2ecce12c5501c35c07
danielbossier <dbossier21@gmail.com>
2022-01-26 13:29:53 -0800

    added has_many and belongs_to relationships to all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+
+  has_many :teams
+  has_many :team_users
+  has_many :teams, through: :team_users
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
